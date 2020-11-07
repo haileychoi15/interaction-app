8f0ce882d765e2e36b44644c1940b8303fb37c89
yuyeong choi <imdud0612@gmail.com>
2020-11-07 22:54:40 +0900

    create infinite Sroll componet

--- src/routes/InfiniteScroll.js
+++ src/routes/InfiniteScroll.js
@@ -1,39 +1,60 @@
-import React, {useCallback, useEffect, useState} from "react";
-import styled, {css} from "styled-components";
+import React, {useCallback, useEffect, useMemo, useRef, useState} from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+const Li = styled.li`
+  display: inline-flex;
+  flex-wrap: wrap;
+`;
 
 function InfiniteScroll() {
-    const initialItems = [
-        {
-            title: '',
-            url: ''
-        },
-        {
-            title: '',
-            url: ''
-        },
-        {
-            title: '',
-            url: ''
-        },
-        {
-            title: '',
-            url: ''
-        },
-        {
-            title: '',
-            url: ''
-        },
-    ];
 
-    const [items, setItems] = useState(initialItems);
-
-    const getScrollPercent = useCallback(() => {
-
+    const getImages = useCallback(async (page) => {
+        console.log(page);
+        const appkey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+        const text = "sky";
+        const photosCount = 10;
+        const http = axios.create({
+            baseURL: "https://api.unsplash.com",
+            headers: {
+                Authorization: `Client-ID ${appkey}`
+            }
+        });
+        const { data: { results } } = await http.get(`/search/photos?query=${text}&page=${page}&per_page=${photosCount}`);
+        const items = results.map((result) => {
+            return {
+                title: result.alt_description,
+                url: result.urls.small
+            }
+        });
+        return items;
     }, []);
 
-    const init = useCallback(() => {
-        window.addEventListener('scroll', getScrollPercent);
-    }, [getScrollPercent]);
+    const [items, setItems] = useState([]);
+    const count = useRef(0);
+
+    const getItems = useCallback( async () => {
+        count.current += 1;
+        if (count.current > 10) {
+            return false;
+        }
+        const newItems = await getImages(count.current + 1);
+        setItems(items => [...items, ...newItems]);
+    }, [getImages]);
+
+    const getScrollValue = useCallback(() => {
+        const scrollY = window.scrollY;
+        const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+        if (scrollY === scrollHeight) {
+            getItems();
+        }
+    }, [getItems]);
+
+    const init = useCallback(async () => {
+        window.addEventListener('scroll', getScrollValue);
+        const items = await getImages(1);
+        setItems(items);
+    }, [getScrollValue]);
 
     useEffect(() => {
         init();
@@ -46,12 +67,13 @@ function InfiniteScroll() {
                 <article>
                     <ul>
                         {items.map((item, index) =>
-                        <li key={index}>
-                            <img src={item.url} alt={item.title} title={item.title}/>
-                        </li>
+                        <Li key={index}>
+                            <img src={item.url} alt={item.title} title={item.title} />
+                        </Li>
                         )}
                     </ul>
                 </article>
+                <footer>footer</footer>
             </section>
         </>
     );
